6cf987debe1511976fb570e204c2caf694af94d5
hannah <hannahlou3107@gmail.com>
2024-03-14 15:37:11 +0000

    Update App.js with new header

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello? from V2</h1>
+        <h2>This is a website on AWS</h2>
       </header>
     </div>
   );
